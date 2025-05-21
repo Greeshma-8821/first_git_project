298ac094b82fb04b1895698eab2edfc723eeeba8
Greeshma-8821 <greeshmaneela@gmail.com>
2025-05-21 17:30:48 +0530

    another code

---
+++ day_of_the_week.py
@@ -0,0 +1,15 @@
+ask=int(input("enter number between 1 to 7 only:"))
+if ask==1:
+    print("monday")
+elif ask==2:
+    print("tuesday")
+elif ask==3:
+    print("wednesday")
+elif ask==4:
+    print("thursday")
+elif ask==5:
+    print("friday")
+elif ask==6:
+    print("satursday")
+elif ask==7:
+    print("sunday")
