8dff1dcdc2cdabc20a385089e507f22b9451ba35
Greeshma-8821 <greeshmaneela@gmail.com>
2025-05-21 14:46:57 +0530

    new code added

---
+++ login system.py
@@ -0,0 +1,8 @@
+user_name="Greeshma"
+password="gree1234"
+ask=input("Enter you username:")
+if ask=="Greeshma":
+    print("thank you enter password")
+    ask2=input("Enter your password:")
+    elif ask2=="gree1234":
+print 
---
+++ number_guessing_name.py
@@ -0,0 +1,10 @@
+secret=88
+ask=int(input("enter any number:"))
+if ask==88:
+    print("Ohh you guessed right!")
+elif ask>88:
+    print("you guess is too high")
+else :
+    print("you guess is too low")
+
+
