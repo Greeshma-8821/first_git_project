ce5044660d9fa670f5091dadf4755aa01a5373a7
Greeshma-8821 <greeshmaneela@gmail.com>
2025-05-23 17:05:28 +0530

    another code

---
+++ functions_assign1.py
@@ -0,0 +1,14 @@
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+def add():
+    print(a+b)
+def subtract():
+    print(a-b)
+def multiply():
+    print(a*b)
+def divide():
+    print(a/b)
+add()
+subtract()
+multiply()
+divide()  
--- login_system(dict_with_tuple_keys).py
+++ login_system(dict_with_tuple_keys).py
@@ -1,1 +1,10 @@
 users={("john","1234"):"admin",("alice","abcd"):"editor"}
+username=input("enter username:")
+username1=input("enter username")
+password=input("enter password:")
+user={(username,username1),password}
+print(user)
+if user==users.items():
+    print(f"Welcome {username}")
+else:
+    print("invalid login")
