0a7c0bf99e563e6a7454a47f8ff96971ea7bf825
Greeshma-8821 <greeshmaneela@gmail.com>
2025-05-28 11:42:02 +0530

    another code

---
+++ loops5.py
@@ -0,0 +1,13 @@
+number=[5,-1,0,12,-7]
+count=0
+for item in number:
+
+    if item is 0:
+      print("zero found")
+    elif item is 1<=50:
+       print(f"{item} is positive")
+    else :
+       print(f"{item} is negative")
+       
+       
+      
